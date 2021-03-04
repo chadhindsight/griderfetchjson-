af7cd4d6f1a63d44f70a9622d07b0e9df5ac8799
Chad Hinds <chadrickj8@gmail.com>
2021-03-04 09:43:52 -0500

    Fix typo in comment section

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-// Label the type of data that each property references. Pointless
+// Label the type of data that each property references. 
 interface Todo {
     id: number;
     name: string;
