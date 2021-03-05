e70a2547ccc760a552f64688bdf9694e15568969
Chad Hinds <chadrickj8@gmail.com>
2021-03-05 09:19:11 -0500

    Change the property name to be title instead

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-// Label the type of data that each property references. 
+// Label the type of data that each property references 
 interface Todo {
     id: number;
-    name: string;
+    title: string;
     completed: boolean;
 }
 
@@ -13,16 +13,16 @@ axios.get(url).then(response => {
     const todo = response.data as Todo;
 
     const id = todo.id;
-    const name = todo.name;
+    const title = todo.title;
     const completed = todo.completed;
 
-    logTodo(id, name, completed);
+    logTodo(id, title, completed);
 });
 
-const logTodo = (id: number, name: string, completed: boolean) => {
+const logTodo = (id: number, title: string, completed: boolean) => {
     console.log(`
     The Todo with ID: ${id}
-    Has a name of: ${name}
+    Has a title of: ${title}
     Is it finished? ${completed}
   `);
 };
